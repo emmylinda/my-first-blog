144798d4426c0165616f9b86a1212b1597f9759d
emmylinda <surya.emmylinda@gmail.com>
2019-10-05 14:28:06 +0200

    Changed the HTML for the site

---
+++ pycode.py
@@ -0,0 +1,18 @@
+test_dict = {
+    'event': 'django_girls',
+    'date': 'today',
+    'participants': 50}
+
+print(test_dict['event'])
+
+test_dict['location'] = 'Berlin'
+
+print(test_dict)
+
+def sum(a, b):
+    return a+b
+print(sum(5,10))
+
+c = sum(5, 10)
+print(c)
+
